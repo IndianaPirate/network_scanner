930f8fa2cac241cc1e684752f99c3a45302e1525
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-11-05 20:24:02 +0300

    feat: add arguments function

--- network_s.py
+++ network_s.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
 import scapy.all as scapy
+import argparse
+
+
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP range")
+    options = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -18,4 +26,6 @@ def display(result):
     for client in result:
         print(client["ip"]+"\t\t"+client["mac"])
 
-display(scan("192.168.43.1/24"))
+options = arguments()
+result = scan(options.target)
+display(result)
