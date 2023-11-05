9036d1c9e1ddf4e841bbec850859fdbd504e3ca5
NIAINA Andriharisoa <niaina-akata@goavana.com>
2023-11-05 20:01:12 +0300

    feat: refactoring and display function

--- network_s.py
+++ network_s.py
@@ -6,7 +6,16 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    answered = scapy.srp(arp_request_broadcast, timeout=10)[0]
+    client_list = []
+    for elem in answered:
+        client = {"ip": elem[1].psrc, "mac": elem[1].hwsrc}
+        client_list.append(client)
+    return client_list
 
-scan("192.168.43.1/24")
+def display(result):
+    print("IP\t\t\tMAC Address\n------------------------------------------------")
+    for client in result:
+        print(client["ip"]+"\t\t"+client["mac"])
+
+display(scan("192.168.43.1/24"))
